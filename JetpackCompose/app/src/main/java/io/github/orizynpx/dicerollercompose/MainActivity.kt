42e8169a847adaaa888f47e6919a91ebf4802abb
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-04-04 12:12:44 +0800

    Add: repo initialized with basic folder structure

---
+++ JetpackCompose/app/src/main/java/io/github/orizynpx/dicerollercompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.orizynpx.dicerollercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.orizynpx.dicerollercompose.ui.theme.DiceRollerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DiceRollerComposeTheme {
+        Greeting("Android")
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+\# Praktikum Pemrograman Mobile
+
+
+
+\## Paralel 2
+
+
+
+Nama Praktikan: Noor Muhammad Akmal Sulaiman
+
+
+
+NIM Praktikan: 2410817210007
+
+
+
+Asisten Praktikum: Muhammad Erza Raihan
+
