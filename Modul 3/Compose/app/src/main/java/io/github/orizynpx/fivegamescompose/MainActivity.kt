c59398204f037526239a33388c9a7fc105b1fbda
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-05-01 21:52:43 +0800

    refactor(modul_3): change ComponentActivity to AppCompatActivity so that the language switch works

--- Modul 3/Compose/app/src/main/java/io/github/orizynpx/fivegamescompose/MainActivity.kt
+++ Modul 3/Compose/app/src/main/java/io/github/orizynpx/fivegamescompose/MainActivity.kt
@@ -1,13 +1,13 @@
 package io.github.orizynpx.fivegamescompose
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import io.github.orizynpx.fivegamescompose.ui.navigation.FiveGamesNavGraph
 import io.github.orizynpx.fivegamescompose.ui.theme.FiveGamesComposeTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
